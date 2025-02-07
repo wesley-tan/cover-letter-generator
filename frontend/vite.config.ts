d34bf0c0113fea9bfca76c355be9eb8282821bdb
Wesley Tan <wesleysimeontan@gmail.com>
2025-02-06 22:07:31 -0500

    edited config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // Expose to all networks
+    host: 'https://cover-letter-generator-frontend.onrender.com/',
     port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
   },
   resolve: {
